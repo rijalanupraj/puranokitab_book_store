a382afe93539e2e24570403a5ea5708683b6ba48
anoop1414 <rijalanupraj@gmail.com>
2020-06-01 10:07:52 +0545

    making proudcts list view and detail view

--- products/views.py
+++ products/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView,DetailView
+from .models import Product
 # Create your views here.
+
+class ProductListView(ListView):
+    queryset = Product.objects.all()
+    template_name = "products/products_list.html"
+
+class ProductDetailView(DetailView):
+    queryset = Product.objects.all()
+    template_name = "products/products_detail.html"
--- puranokitab/settings.py
+++ puranokitab/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'puranokitab.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "accounts/templates"),os.path.join(BASE_DIR, "home/templates"),os.path.join(BASE_DIR, "products/templates"),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
