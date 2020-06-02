0ca39eea95558c6181fce40b70971fa1bc1c7354
anoop1414 <rijalanupraj@gmail.com>
2020-06-02 13:58:53 +0545

    made model of cart and did migration

--- carts/models.py
+++ carts/models.py
@@ -1,3 +1,16 @@
+from django.conf import settings
 from django.db import models
+from products.models import Product
+
+User = settings.AUTH_USER_MODEL
 
 # Create your models here.
+class Cart(models.Model):
+    user = models.ForeignKey(User, null=True,blank=True,on_delete=models.CASCADE)
+    products = models.ManyToManyField(Product,blank=True)
+    total = models.DecimalField(default=0.0,max_digits=60,decimal_places=2)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.id)
