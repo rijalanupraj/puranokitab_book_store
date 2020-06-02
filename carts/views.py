4a9ef1491d5f20b800a47a240fb79d8ca3079feb
anoop1414 <rijalanupraj@gmail.com>
2020-06-02 15:48:24 +0545

    simple cart session managed

--- carts/views.py
+++ carts/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 
+from .models import Cart
 # Create your views here.
 
 def cart_home(request):
-    # print(dir(request.session))
-    
+    cart_id = request.session.get("cart_id",None)
+    qs = Cart.objects.filter(id=cart_id)
+    if qs.count() == 1:
+        print('Card Id exists')
+        cart_obj = qs.first()
+        if request.user.is_authenticated and cart_obj.user is None:
+            cart_obj.user = request.user
+            cart_obj.save()
+    else:
+        cart_obj = Cart.objects.new(user=request.user)
+        request.session['cart_id'] = cart_obj.id
     return render(request,"carts/home.html")
