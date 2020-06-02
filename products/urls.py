9b1a1fe5f4d9d952013693b5e57e9a4d4ecdfff0
anoop1414 <rijalanupraj@gmail.com>
2020-06-02 09:30:11 +0545

    about to start search

--- products/urls.py
+++ products/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.ProductListView.as_view(),name='product-home'),
+    path('',views.ProductListView.as_view(),name='products-main-page'),
     path('<str:slug>/',views.ProductDetailView.as_view(),name='product-detail'),
     path('featured/',views.ProductFeaturedListView.as_view(),name='fddf'),
 ]
