2a883564141b73579cfa14fd428f9f79394ffdc3
anoop1414 <rijalanupraj@gmail.com>
2020-06-02 21:07:01 +0545

    cart function managed

--- carts/views.py
+++ carts/views.py
@@ -6,7 +6,7 @@ from .models import Cart
 def cart_home(request):
     cart_obj, new_obj = Cart.objects.new_or_get(request)
     
-    return render(request,"carts/home.html")
+    return render(request,"carts/home.html",{"cart":cart_obj})
 
 def cart_update(request):
     product_id = request.POST.get('product_id')
