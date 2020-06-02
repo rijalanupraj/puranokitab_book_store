8b2c9fe839a44058ef53c076abe260aca5357595
anoop1414 <rijalanupraj@gmail.com>
2020-06-02 19:16:34 +0545

    working on cart

--- carts/views.py
+++ carts/views.py
@@ -12,6 +12,9 @@ def cart_update(request):
     product_id = 11
     product_obj = Product.objects.get(id=product_id)
     cart_obj,new_obj = Cart.objects.new_or_get(request)
-    cart_obj.products.add(product_obj)
+    if product_obj in cart_obj.products.all():
+        cart_obj.products.remove(product_obj)
+    else:
+        cart_obj.products.add(product_obj)
     return redirect("cart:cart-home")
     
