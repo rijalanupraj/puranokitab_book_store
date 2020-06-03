7b30bf36cd910fda7a6d43a563bca62a47515711
anoop1414 <rijalanupraj@gmail.com>
2020-06-03 18:32:47 +0545

    address taken from the user and added to database

--- puranokitab/urls.py
+++ puranokitab/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+from addresses.views import checkout_address_create_view
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/',include('accounts.urls')),
@@ -24,6 +25,7 @@ urlpatterns = [
     path('products/', include('products.urls',namespace='products')),
     path('search/',include('search.urls',namespace='search')),
     path('cart/',include('carts.urls',namespace='cart')),
+    path('checkout_address_create_view/',checkout_address_create_view,name='checkout_address_create_view'),
 
 ]
 if settings.DEBUG:
