73bb5da2c15e62486dc489d8f099174ae92634dc
anoop1414 <rijalanupraj@gmail.com>
2020-06-03 10:52:17 +0545

    checkout on progress

--- carts/urls.py
+++ carts/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('',views.cart_home,name='cart-home'),
     path('update/',views.cart_update,name='update'),
+    path('checkout/',views.checkout_home,name='checkout'),
 
 ]
 
